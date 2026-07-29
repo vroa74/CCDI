59d2e65ec3115d9f636eb7889df8304a9f0bba29
vroa74 <33233830+vroa74@users.noreply.github.com>
2026-07-29 13:46:29 -0600

    V_0003
    se realizo modificaciones a la pagina de dashboard y se agregaron los controladores del modelo anterior.

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -18,7 +18,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Dashboard') }} 
                     </x-nav-link>
                     {{-- <x-nav-link href="{{ route('usuarios.index') }}" :active="request()->routeIs('usuarios.index')">
                         <i class="ri-group-3-fill"></i>   {{ __('Usuarios (CRUD)') }}
